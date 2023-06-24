664c0a0e25f62fdf947a6ffd8db1e1338a3ee49e
KotBeginmot <iliynaea@gmail.com>
2023-06-24 14:30:38 +0300

    add try/except in function

--- src/video.py
+++ src/video.py
@@ -4,17 +4,25 @@ import os
 
 class Video:
     """Функция для получения информации о видео с YouTube"""
-    #API_KEY = os.getenv('YT_KEY')
+    # API_KEY = os.getenv('YT_KEY')
     API_KEY = "AIzaSyAhWpNBvT-JtKHFdF0tM1ksiLSNTeT2SKs"
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, id_video):
-        self.id_video = id_video
-        self.video = self.youtube.videos().list(id=self.id_video, part='snippet,statistics').execute()
-        self.title_video = self.video['items'][0]['snippet']['title']
-        self.url_video = self.video['items'][0]['snippet']['thumbnails']['default']['url']
-        self.count_views = self.video['items'][0]['statistics']['viewCount']
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+
+        try:
+            self.id_video = id_video
+            self.video = self.youtube.videos().list(id=self.id_video, part='snippet,statistics').execute()
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url_video = self.video['items'][0]['snippet']['thumbnails']['default']['url']
+            self.count_views = self.video['items'][0]['statistics']['viewCount']
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video = None
+            self.title = None
+            self.url_video = None
+            self.count_views = None
+            self.like_count = None
 
     def __str__(self):
         """Функция вывода названия видео"""
